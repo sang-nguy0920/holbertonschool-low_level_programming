6e14bc8269d73c84bb146fee7a9737dd88854708
Sang Nguyen <2321@holbertonschool.com>
2020-09-16 20:44:31 +0000

    Added file 5-printf.c in 0x00-hello_world DIR v1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - prints a string followed by a new line
+ *
+ *Return: returns to int
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
