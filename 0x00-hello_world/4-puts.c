06b1ed25cc29d758142efc6c9405c153e3a5a1b4
Sang Nguyen <2321@holbertonschool.com>
2020-09-16 20:37:01 +0000

    Corrected file 4-puts.c in 0x00-hello_world DIR v4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main - uses puts to print a string
+ *
+ *Return: returns to int
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
