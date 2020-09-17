16c33c50df03bab333f17066c300d57ec17b961e
Sang Nguyen <2321@holbertonschool.com>
2020-09-17 21:18:34 +0000

    Added code in 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints single digit # of base 10 start from 0 using putchar
+ *
+ *Return: returns int
+ */
+
+int main(void)
+{
+int c;
+c = 48;
+while (c <= 57)
+{
+putchar (c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+c = c + 1;
+}
+putchar ('\n');
+
+return (0);
+}
