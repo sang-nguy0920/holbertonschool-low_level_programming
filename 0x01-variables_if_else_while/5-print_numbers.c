3421acc49eb9d9d949c5c16049ab41606cb345d7
Sang Nguyen <2321@holbertonschool.com>
2020-09-17 20:51:34 +0000

    Added code in 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - prints single digit # of base 10 start from 0
+ *
+ *Return: returns int
+ */
+
+int main(void)
+{
+int a10;
+
+while (a10 < 10)
+{
+printf("%d", a10);
+a10++;
+}
+putchar ('\n');
+return (0);
+}
