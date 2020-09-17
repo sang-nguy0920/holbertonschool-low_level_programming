f7f135c9d9113c96c288fbcc02c409c00e3621dd
Sang Nguyen <2321@holbertonschool.com>
2020-09-17 20:17:55 +0000

    Added code to 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - prints alphabet in lowercase, followed by a new line
+ *
+ *Return: returns int
+ */
+
+int main(void)
+{
+char c;
+c = "a";
+while (c <= 'z')
+{
+putchar (c);
+c = c + 1;
+}
+putchar ('\n');
+
+return (0);
+}
