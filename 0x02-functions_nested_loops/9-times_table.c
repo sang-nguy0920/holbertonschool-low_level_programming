e23a0a75641c2ef62aefd9eae3f60ae578c2eb33
Sang Nguyen <2321@holbertonschool.com>
2020-09-22 20:54:57 +0000

    Fixed file 9-times_tabl.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,12 @@ for (x = 0; x <= 9; x++)
 for (y = 0; y <= 9; y++)
 {
 z = x * y;
-if (z >= 10)
+if (y == 9 && z >=10)
+{
+_putchar(z / 10 % 10 + '0');
+_putchar(z  % 10 + '0');
+}
+else if (z >= 10)
 {
 _putchar(z / 10 % 10 + '0');
 _putchar(z  % 10 + '0');
@@ -27,7 +32,7 @@ _putchar(' ');
 else if (y == 9)
 {
 _putchar(' ');
-_putchar(z % 10 + '0');
+_putchar((z % 10) + '0');
 }
 else
 {
