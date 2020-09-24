cbc53134157804738f0339132224e2df27c70f31
Sang Nguyen <2321@holbertonschool.com>
2020-09-24 22:11:26 +0000

    Added code to file 10-print_triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *print_square - prints a triangle, followed by a new line.
+ *@n: size
+ *
+ *Description: prints a triangle, followed by a new line.
+ *section header: Section description
+ *Return: returns void
+ */
+void print_triangle(int n)
+{
+int y, z;
+if (n <= 0)
+_putchar('\n');
+for (y = 1; y <= n; y++)
+{
+for (z = 1; z <= n; z++)
+{
+if (z <= (n -y))
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
