d87b4f78608a6de9ba435ec7b6d4b1dc5df08fc6
Sang Nguyen <2321@holbertonschool.com>
2020-09-25 19:25:26 +0000

    Add code to file 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,20 @@
  */
 void print_number(int n)
 {
-unsigned int a;
+unsigned int b;
 
-
+if (n < 0)
+{
+b = -n;
+_putchar('-');
+}
+else
+{
+b = n;
+}
+if (b / 10)
+{
+print_number(b / 10);
+}
+_putchar((b % 10) + '0');
 }
