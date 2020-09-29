3c53476c2f1d90fc72dec19c30074cf0b345871d
Sang Nguyen <2321@holbertonschool.com>
2020-09-29 14:51:08 +0000

    Fixed code in file 6-puts2.c  v2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,7 +13,13 @@ void puts2(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i += 2)
+{
 _putchar(str[i]);
+}
+if (str[i] != '\0' && str[i] % 2 != 0)
+{
+_putchar(str[i]);
+i++;
+}
 _putchar('\n');
-
 }
