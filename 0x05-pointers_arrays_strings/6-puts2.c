3c5f366afe86c5c2e54d60d4fe1db61785c4d1fc
Sang Nguyen <2321@holbertonschool.com>
2020-09-29 00:20:49 +0000

    Added code to file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *puts2 - prints every other character of a string 
+ *@str: *str pointer
+ *
+ *Description: prints every other character of a string 
+ *section header: Section description
+ *Return: returns void
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+_putchar(str[i]);
+_putchar('\n');
+
+}
