0db822e87991871edb970c91c58a096c7ce24d8d
Sang Nguyen <2321@holbertonschool.com>
2020-09-29 00:43:10 +0000

    Fixed code in file 7-puts_half.c v1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- *puts_half - function that prints half of a string 
+ *puts_half - function that prints half of a string
  *@str: *str pointer
  *
- *Description: function that prints half of a string 
+ *Description: function that prints half of a string
  *section header: Section description
  *Return: returns void
  */
