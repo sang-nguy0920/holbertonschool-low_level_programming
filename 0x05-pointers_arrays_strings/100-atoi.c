69a0459c92d1b892d1c6e6a852239452af954779
Sang Nguyen <2321@holbertonschool.com>
2020-09-29 18:23:55 +0000

    Fixed code in file 8-print_array.c v3

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_atoi - function that convert a string to an integer.
+ *@s: *s pointer
+ *
+ *Description: function that convert a string to an integer.
+ *section header: Section description
+ *Return: returns integer
+ */
+
+int _atoi(char *s)
+{
+int n;
+printf("%s\n", n);
+
+}
