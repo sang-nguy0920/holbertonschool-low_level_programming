10ee7947f1526a0f7c94c2c79575149dc770646c
Sang Nguyen <2321@holbertonschool.com>
2020-10-01 17:57:26 +0000

    Fixed code in file 2-strncpy.c v1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,22 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+int i, j;
 
-
-
+i = 0;
+j = 0;
+while (i != n)
+{
+dest[j] = src[i];
+if (src[i] == '\0')
+{
+dest[j] = '\0';
+break;
+}
+j++;
+i++;
+}
+while (j != n)
+dest[j++] = '\0';
+return (dest);
 }
