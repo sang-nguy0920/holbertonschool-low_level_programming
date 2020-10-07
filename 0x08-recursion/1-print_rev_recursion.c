ba68573fcc0aa548a284ee4bb609cb0cce962d35
Sang Nguyen <2321@holbertonschool.com>
2020-10-07 18:18:28 +0000

    Added code to file 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - function that prints a string in reverse.
+ *@s: *s pointer
+ *
+ *Description: function that prints a string in reverse.
+ *section header: Section description
+ *Return: returns void
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
