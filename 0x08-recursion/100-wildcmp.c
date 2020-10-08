44c899df8e3e5ad246bb8178013248c64dafadb5
Sang Nguyen <2321@holbertonschool.com>
2020-10-08 18:26:44 +0000

    Added code to file 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -13,6 +13,20 @@
 
 int wildcmp(char *s1, char *s2)
 {
-
+if (*s1 == *s2 && *s1 && *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) != *s1 && *(s2 + 1) != '*')
+			return (wildcmp(s1 + 1, s2));
+		else if (*(s2 + 1) != *s1 && *(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (!wildcmp(s1, s2 + 1))
+			return (wildcmp(s1 + 1, s2));
+		if (wildcmp(s1, s2 + 1))
+            return (1);
+    }
+    return (0);
 }
-
