2368de9113e08b29feef60bdef1abe704abea481
Sang Nguyen <2321@holbertonschool.com>
2020-10-08 18:28:31 +0000

    Added code to file 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- *wildcmp - function that compares two identical strings. Returns 1 is true. 
+ *wildcmp - function that compares two identical strings.
  *@s1: *s1 pointer
  *@s2: *s2 pointer
  *
- *Description: function that compares two identical strings. Returns 1 is true. 
+ *Description: function that compares two identical strings.
  *section header: Section description
  *Return: returns int
  */
@@ -14,19 +14,24 @@
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == *s2 && *s1 && *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (!*s1 && !*s2)
-		return (1);
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) != *s1 && *(s2 + 1) != '*')
-			return (wildcmp(s1 + 1, s2));
-		else if (*(s2 + 1) != *s1 && *(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		if (!wildcmp(s1, s2 + 1))
-			return (wildcmp(s1 + 1, s2));
-		if (wildcmp(s1, s2 + 1))
-            return (1);
-    }
-    return (0);
+return (wildcmp(s1 + 1, s2 + 1));
+
+if (!*s1 && !*s2)
+return (1);
+
+if (*s2 == '*')
+{
+if (*(s2 + 1) != *s1 && *(s2 + 1) != '*')
+return (wildcmp(s1 + 1, s2));
+
+else if (*(s2 + 1) != *s1 && *(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+
+if (!wildcmp(s1, s2 + 1))
+return (wildcmp(s1 + 1, s2));
+
+if (wildcmp(s1, s2 + 1))
+return (1);
+}
+return (0);
 }
