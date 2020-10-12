24b9745b7a3ff518791c5f7ae03b976208f416c2
Sang Nguyen <2321@holbertonschool.com>
2020-10-12 21:51:22 +0000

    Fixed code to file 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,60 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *main -  prints the minimum number of coins to make change for an amount.
- *@argc: number of args
- *@argv: *array
+ * main - Prints the minimum number of coins to
  *
- *Description:  prints the minimum number of coins to make change for an amount.
- *section header: Section description
- *Return: returns int
- */
-
-int main (int argc, char *argv[])
+ * @argc: number of arguments given to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one return -1.
+ * Otherwise return 0
+*/
+int main(int argc, char *argv[])
 {
-int x;
-int y = atoi(argv[1]);
-
-if (argc == 2)
+int coins, total;
+if (argc != 2)
 {
-while (y >= 0)
-{
-if (y < 0)
-{
-break;
-}
-if (y % 25 == 0)
-{
-y -= 25;
-x++;
-}
-else if (y % 10 == 0)
-{
-y -= 10;
-x++;
-}
-else if (y % 5 == 0)
-{
-y -= 5;
-x++;
-}
-else if (y % 2 == 0 && y / 2 != 3)
-{
-y -= 2;
-x++;
-}
-else if (y % 1 == 0)
-{
-y -= 1;
-x++;
-}
-}
-}
-else
-{
-printf ("Error\n");
+printf("Error\n");
 return (1);
 }
-printf ("%d\n", x);
+total = atoi(argv[1]);
+if (total < 1)
+{
+printf("0\n");
+return (0);
+}
+while (total != 0)
+{
+if (total >= 25)
+total -= 25;
+else if (total >= 10)
+total -= 10;
+else if (total >= 5)
+total -= 5;
+else if (total >= 2)
+total -= 2;
+else
+total -= 1;
+coins++;
+}
+printf("%d\n", coins);
 return (0);
 }
