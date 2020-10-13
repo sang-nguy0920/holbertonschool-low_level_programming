1a2d0ffc51c6f206b41b53671bdd018f27008783
Sang Nguyen <2321@holbertonschool.com>
2020-10-13 18:01:50 +0000

    Added code to file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - function that returns a pointer to a newly allocated space in mem
+ *@str: *str pointer
+ *
+ *Description: function that return a pointer to a newly allocated space in mem
+ *section header: Section description
+ *Return: returns char
+ */
+
+char *_strdup(char *str)
+{
+char *p;
+int i;
+int j;
+
+if (str == NULL)
+return (NULL);
+
+for (j = 0 ; str[j] != '\0' ; j++)
+
+p = (char *)malloc(sizeof(char) * (j + 1));
+
+if (p == NULL)
+return (NULL);
+
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+p[i] = str[i];
+}
+return (p);
+}
