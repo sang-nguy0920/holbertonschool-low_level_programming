b6ebef0ada31121c6c80ae95d833948c21292c72
Sang Nguyen <2321@holbertonschool.com>
2020-10-19 20:38:52 +0000

    Fixed code in file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,7 +23,14 @@ printf("Name: (nil)\n");
 else
 {
 printf("Name: %s\n", d->name);
-printf("Name: %f\n", d->age);
+}
+if (d->age < 0)
+{
+printf("Age: (nil)\n");
+}
+else
+{
+printf("Age: %f\n", d->age);
 }
 if (d->owner == NULL)
 {
@@ -31,6 +38,6 @@ printf("Owner: (nil)\n");
 }
 else
 {
-printf("Name: %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
 }
