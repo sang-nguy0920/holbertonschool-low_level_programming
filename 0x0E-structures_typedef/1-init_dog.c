01f10e4145307db0294ff9a7790b8bdaa2164c9a
Sang Nguyen <2321@holbertonschool.com>
2020-10-19 20:12:51 +0000

    Added prototype for 1-init_dog.c in dog.h and code to file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *init_dog - function that initialize a variable of type struct dog
+ *@d: *d pointer
+ *@name: *name pointer
+ *@age: float age
+ *@owner: *owner pointer
+ *
+ *Description: function that initialize a variable of type struct dog
+ *section header: Section description
+ *Return: returns void
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
