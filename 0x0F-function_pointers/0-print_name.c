2b7cf09ca53ef32b222075cb26930ee38471c006
Sang Nguyen <2321@holbertonschool.com>
2020-10-21 19:34:39 +0000

    Added code to file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - function that prints a name.
+ *@name: *name pointer
+ *@f: *f function pointer
+ *
+ *Description: function that prints a name.
+ *section header: Section description
+ *Return: returns void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f == 0 || name == 0)
+{
+return;
+}
+f(name);
+}
