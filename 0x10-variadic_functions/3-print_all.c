25214f90aced2ef03b7f4e8ac0e4db21a59d7beb
Sang Nguyen <2321@holbertonschool.com>
2020-10-23 19:17:13 +0000

    Added code to file 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+
+/**
+ *print_all -  function that prints anything.
+ *@separator: *seperator pointer
+ *@n: const unsigned int
+ *
+ *Description:  function that prints anything.
+ *section header: Section description
+ *Return: returns void
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list arg;
+
+unsigned int x = 0;
+
+while (format)
+{
+va_start(arg, format);
+while (format[x] != '\0')
+{
+switch (format[x])
+{
+case 'c': printf("%c, ", va_arg(arg, int)); 
+break;
+       
+case 'i': printf("%d, ", va_arg(arg, int)); 
+break;
+     
+case 'f': printf("%f", va_arg(arg, double)); 
+break;
+    
+case 's': printf("%s", va_arg(arg, char *)); 
+break;
+       
+default: 
+break;
+}
+x++;
+}
+va_end(arg);
+break;
+}
+printf("\n");
+}
