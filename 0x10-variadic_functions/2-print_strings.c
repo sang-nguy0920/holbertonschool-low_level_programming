0641657a50340233e10d49ddb51af66b085404e9
Sang Nguyen <2321@holbertonschool.com>
2020-10-23 17:30:48 +0000

    Added code to file 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - function that prints numbers, followed by a new line.
+ *@separator: *seperator pointer
+ *@n: const unsigned int
+ *
+ *Description: function that prints numbers, followed by a new line.
+ *section header: Section description
+ *Return: returns void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+va_list arg;
+unsigned int i;
+
+va_start(arg, n);
+for (i = 0; i < n; i++)
+{
+printf("%s", va_arg(arg, char *));
+if (separator != NULL)
+{
+if (i < n - 1)
+printf("%s", separator);
+}
+if (arg == NULL)
+printf("(nil)\n");
+}
+putchar('\n');
+va_end(arg);
+}
