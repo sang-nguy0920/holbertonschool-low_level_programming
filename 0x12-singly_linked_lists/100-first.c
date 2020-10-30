4de89c401a11e301d2ec960f025cd6a3ee10e6df
sang-nguy0920 <2321@holbertonschool.com>
2020-10-30 19:45:42 +0000

    Added code to file 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ *print_first - function that prints str before main func
+ *
+ *
+ *Description: function that prints str before main func
+ *section header: Section description
+ *Return: void
+ */
+
+void __attribute__ ((constructor)) print_first(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
