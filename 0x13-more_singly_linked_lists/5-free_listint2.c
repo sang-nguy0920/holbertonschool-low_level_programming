fa7477c90d8311153e7da4e9ec0dae6b2c7cb5f6
sang-nguy0920 <2321@holbertonschool.com>
2020-11-04 17:52:21 +0000

    Edited 5-free_listint2.c for betty compliance.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,13 @@ void free_listint2(listint_t **head)
 {
 listint_t *tmp;
 
-while (head != NULL)
+if (head)
 {
-tmp = head;
-head = head->next;
+while (*head)
+{
+tmp = *head;
+*head = tmp->next;
 free(tmp);
+}
 }
 }
