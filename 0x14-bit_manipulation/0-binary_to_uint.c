d51d6c60e4d5ace94844aa0418eae8d75a297f20
sang-nguy0920 <2321@holbertonschool.com>
2020-11-05 21:08:37 +0000

    Added code to file 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *binary_to_uint - function that converts a binary number to an unsigned int.
+ *@b: *b pointer to string of 0 and 1 chars
+ *
+ *Description: function that converts a binary number to an unsigned int.
+ *section header: Section description
+ *Return: a converted number else 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int x =0, num = 0;
+
+if (b == NULL)
+return (0);
+
+while (b[x] != '\0')
+{
+if (b[x] != '0' && b[x] != '1')
+return (0);
+num = num << 1;
+if (b[x] == '1')
+num = num ^ 1;
+x++;
+}
+return (num);
+}
