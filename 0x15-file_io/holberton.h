d076dc600774a1a3087012559bd9f732939b0dbc
sang-nguy0920 <2321@holbertonschool.com>
2020-11-09 20:54:00 +0000

    Created directory 0x15-file_io, empty project files, _putchar.c, holberton.h, and README.md

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H15
+#define HOLBERTON_H15
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+
+#endif /* HOLBERTON_H */
