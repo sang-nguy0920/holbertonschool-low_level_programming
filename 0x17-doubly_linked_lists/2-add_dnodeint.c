51bfc6b059520c897be09848b384380f047c1768
sang-nguy0920 <2321@holbertonschool.com>
2020-12-14 00:42:43 +0000

    Added files for 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,16 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
+dlistint_t *temp = NULL, *new_node = NULL;
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 new_node->n = n;
 new_node->prev = NULL;
-new_node->next = *head;
-(*head) = new_node;
+temp = *head;
+*head = new_node;
+new_node->next = temp;
+if (temp != NULL)
+temp->prev = *head;
 return (new_node);
 }
