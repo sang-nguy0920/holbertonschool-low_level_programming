0c8efc8d85d3baba21fd319ff5ba10cac2890954
sang-nguy0920 <2321@holbertonschool.com>
2021-01-16 02:20:15 +0000

    It's a dictionary

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that deletes a hash table.
+ * @ht: the hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+hash_node_t *node, *tmp;
+unsigned long int size, x = 0;
+
+if (ht && ht->array)
+{
+size = ht->size;
+while (x < size)
+{
+node = (ht->array)[x];
+while (node)
+{
+tmp = node->next;
+if (node->key)
+free(node->key);
+if (node->value)
+free(node->value);
+free(node);
+node = tmp;
+}
+x++;
+}
+free(ht->array);
+free(ht);
+}
+}
